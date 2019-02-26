6cd4030faf097bece62eb76182ef69505860cff9
Micah Hobbs <micahthobbs@gmail.com>
2019-02-26 20:04:34 +1100

    Form validation and errors flags

--- js/script.js
+++ js/script.js
@@ -16,18 +16,23 @@ $(`#title`).change(function(){
     Configure t-shirt info section options
 */ 
 
+$(`#colors-js-puns`).hide();
+
 $(`#design`).change(function(){
     $(`.love-js`).show();
     $(`.js-puns`).show();
     const $designSelected = $(`#design`).val();
     if ($designSelected === `js puns`){
+        $(`#colors-js-puns`).show();
         $(`.love-js`).hide();
         $(`#color option`).eq(1).prop(`selected`, true);
     } else if ($designSelected === `heart js`){
+        $(`#colors-js-puns`).show();
         $(`.js-puns`).hide();
         $(`#color option`).eq(3).prop(`selected`, true);
     } else {
         $(`#color option`).eq(0).prop(`selected`, true);
+        $(`#colors-js-puns`).hide();
     }
 });
 
@@ -68,6 +73,7 @@ $(`.activities input:checkbox`).change(function(){
     // Display total element
     if (totalCost > 0) {
         activitiesTotalDiv.classList.remove(`is-hidden`)
+        activitiesWarningMessage.textContent = ``;
         activitiesP.textContent = `Total Amount: $${totalCost}`
     } else {
         activitiesTotalDiv.classList.add(`is-hidden`)
@@ -100,27 +106,66 @@ $(`#payment`).change(function() {
 /*
     Form validation
 */
+    
+    // Create Activity warning message elements
+    const activitiesWarningMessage = document.createElement(`p`);
+    activitiesTotalDiv.appendChild(activitiesWarningMessage);
 
-$(`#name`).change(function(event){
-    let nameInput = this.value; 
+$(`form`).submit(function(event){
+
+    // Validate name field not blank
     const nameRegex = /^$|\s+/;
-    if (regexParser(nameInput, nameRegex)){
-        warningMessage(event)
+    const nameInput = $(`#name`).val()
+    if (nameRegex.test(nameInput)){
+        event.preventDefault();
+        $(`#name`).css(`border`, `2px solid red`);
     } else {
-        validatedMessage(event)
+        event.preventDefault();
+        $(`#name`).css(`border`, ``);
     }
+
+    // Validate email address
+    const emailRegex = /^\w*@\w*\.com$/;
+    const emailInput = $(`#mail`).val()
+    if (emailRegex.test(emailInput)){
+        event.preventDefault();
+        $(`#mail`).css(`border`, ``);
+    } else {
+        event.preventDefault();
+        $(`#mail`).css(`border`, `2px solid red`);
+    }
+
+    // Check user selected at least one activity checkbox
+    if ($(`.activities input[type=checkbox]:checked`)) {
+        activitiesWarningMessage.textContent = `Please selection at least 1 activity`; 
+    }
+
+    // If payment credit card
+    // Make sure CC number between 13 and 15
+    const ccNumRegex = /^\d{13,16}$/;
+    const ccNumInput = $(`#cc-num`).val();
+    if (ccNumRegex.test(ccNumInput)) {
+        $(`#cc-num`).css(`border`, ``);
+    } else {
+        $(`#cc-num`).css(`border`, `2px solid red`);
+    }
+
+    // Zip code is a 5 digit number
+    const ccZipRegex = /^\d{3,5}$/;
+    const ccZipInput = $(`#zip`).val();
+    if (ccZipRegex.test(ccZipInput)) {
+        $(`#zip`).css(`border`, ``);
+    } else {
+        $(`#zip`).css(`border`, `2px solid red`);
+    }
+
+    const ccCVVRegex = /^\d{3}$/;
+    const ccCVVInput = $(`#cvv`).val();
+    if (ccCVVRegex.test(ccCVVInput)) {
+        $(`#cvv`).css(`border`, ``);
+    } else {
+        $(`#cvv`).css(`border`, `2px solid red`);
+    }
+
 });
 
-// function to pass regex and form element
-function regexParser(pattern, string) {
-    return pattern.test(string)
-}
-
-// function to pass produce form warnings
-function warningMessage(event) {
-
-}
-
-function validatedMessage(event) {
-
-}
